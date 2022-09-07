ec1a004d7c0219ee06d03a9a412b0b639c107abc
Nolan Corcoran <nd.corc@gmail.com>
2022-09-07 00:17:32 -0500

    Asdf

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,8 +3,8 @@ module.exports = {
     "../**/*.story.@(js|jsx|ts|tsx)",
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
